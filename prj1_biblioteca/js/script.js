ffafb0b947453f4ec449c846fdca96c9c8f9a23d
mha13 <111089847+mha13@users.noreply.github.com>
2025-11-18 20:01:14 +0100

    Update script.js

--- prj1_biblioteca/js/script.js
+++ prj1_biblioteca/js/script.js
@@ -1,12 +1,22 @@
-let liberi = ["Don Chisciotte della Mancia","Harry Potter","Il Piccolo Principe","Il Signore degli Anelli","Le avventure di Pinocchio","Le due città"];
-let autori = ["Miguel de Cervantes","J.K. Rowling","Antoine de Saint-Exupéry","J.R.R. Tolkien","Carlo Collodi","Charles Dickens"];
+let liberi = ["Harry Potter","Il Piccolo Principe","Il Signore degli Anelli","Le avventure di Pinocchio","Le due città","Don Chisciotte della Mancia"];
+let autori = ["J.K. Rowling","Antoine de Saint-Exupéry","J.R.R. Tolkien","Carlo Collodi","Charles Dickens","Miguel de Cervantes"];
+
+
+
+
+
+let nuovoArray=[];
+for (let i = 0 ; i < liberi.length ; i++ ){
+    nuovoArray.push(liberi[i]+" - "+autori[i]);
+}
+nuovoArray.sort();
+nuovoArray.forEach(nuovoItem => {
+    document.writeln(`<p>${nuovoItem}</p>`);
+});
+
+
 
 let mieiFacoriti = ["L'alchimista","Le quaranta porte"];
-
-for (let i = 0 ; i < liberi.length ; i++ ){
-    document.writeln(`<p>${liberi[i]} - ${autori[i]}</p>`);
-}
-
 document.writeln(`<h3>Il mio favoriti</h3>`);
 mieiFacoriti.sort();
 mieiFacoriti.forEach(mioFacorito => {
