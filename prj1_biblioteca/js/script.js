5c130d221db85a618d4e82b5b30ac32fb3f296b1
mha13 <111089847+mha13@users.noreply.github.com>
2025-11-18 20:05:26 +0100

    Update script.js

--- prj1_biblioteca/js/script.js
+++ prj1_biblioteca/js/script.js
@@ -1,10 +1,6 @@
 let liberi = ["Harry Potter","Il Piccolo Principe","Il Signore degli Anelli","Le avventure di Pinocchio","Le due città","Don Chisciotte della Mancia"];
 let autori = ["J.K. Rowling","Antoine de Saint-Exupéry","J.R.R. Tolkien","Carlo Collodi","Charles Dickens","Miguel de Cervantes"];
 
-
-
-
-
 let nuovoArray=[];
 for (let i = 0 ; i < liberi.length ; i++ ){
     nuovoArray.push(liberi[i]+" - "+autori[i]);
@@ -13,8 +9,8 @@ nuovoArray.sort();
 nuovoArray.forEach(nuovoItem => {
     document.writeln(`<p>${nuovoItem}</p>`);
 });
-
-
+//oltre metodo
+console.log(liberi.map((libero,index)=>{return libero+" - "+autori[index]}));
 
 let mieiFacoriti = ["L'alchimista","Le quaranta porte"];
 document.writeln(`<h3>Il mio favoriti</h3>`);
